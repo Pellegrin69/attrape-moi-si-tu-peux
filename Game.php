a6ceeeaf2ff5db579416b295f45d1c51d77ed68b
Pellegrin69 <pellegrinpierre@hotmail.fr>
2023-07-26 12:09:19 +0200

    Testing if the game has two players

--- Game.php
+++ Game.php
@@ -1,5 +1,24 @@
 <?php
 class Game
 {
+    public $playerOne;
+    public $playerTwo;
+
+    function getPlayerOne() {
+        return $this->playerOne;
+    }
+    function getPlayerTwo() {
+        return $this->playerTwo;
+    }
+    function setPlayerOne($player) {
+        $this->playerOne = $player;
+    }
+    function setPlayerTwo($player) {
+        $this->playerTwo = $player;
+    }
+}
+
+class Player
+{
 
 }
--- gameTest.php
+++ gameTest.php
@@ -9,5 +9,17 @@ class GameTest extends TestCase
         $this->assertInstanceOf(Game::class, new Game());
     }
 
+    public function testGameHaveTwoPlayers() {
+        $game = new Game();
+        $game->setPlayerOne(new Player());
+        $game->setPlayerTwo(new Player());
+
+        $playerOne = $game->getPlayerOne();
+        $playerTwo = $game->getPlayerTwo();
+
+        $this->assertInstanceOf(Player::class, $playerOne);
+        $this->assertInstanceOf(Player::class, $playerTwo);
+    }
+
 }
 
